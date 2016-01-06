32462b9565e0e7ecb8baa53f66319e2a5ad13ef4
hifanso <hifanso@163.com>
2016-01-06 18:21:40 +0800

    add edison_tools folder to delete list

--- pub/install_head.sh
+++ pub/install_head.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 line=`wc -l $0|awk '{print $1}'`
 line=`expr $line - 10` 
-rm -r io-js/ Atlas/ node-red/ lib/ README.md install.sh install_for_dev.sh  
+rm -r io-js/ Atlas/ node-red/ lib/ edison_tools/ README.md install.sh install_for_dev.sh  
 tail -n $line $0 |tar xzv
 ./install.sh
 ret=$?
