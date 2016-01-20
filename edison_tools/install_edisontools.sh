e66d1774579c68e084632a64a98d2d2a8a9a385c
Tian Ye <flymaxty@gmail.com>
2016-01-20 16:54:22 +0800

    update install_edisontools.sh

--- edison_tools/install_edisontools.sh
+++ edison_tools/install_edisontools.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo "Installing Edison extension board check tool.\n"
+echo "Installing Edison extension board check tool."
 cp extboardcheck.sh /etc/
 cp all_built/check5611 /usr/bin
 cp all_built/check6050 /usr/bin
@@ -8,7 +8,7 @@ cp all_built/mraaio /usr/bin
 
 systemctl disable extboard_detect
 cp extboard_detect.service /etc/systemd/system/
-echo "Enabling Edison extension board check tool start up.\n"
+echo "Enabling Edison extension board check tool start up."
 systemctl enable extboard_detect
-systemctl restart extboard_detect
+systemctl start extboard_detect
 echo "All done."
