d7d229acd016dd41261a7cb2f022723c9ddf0b38
Tian Ye <flymaxty@gmail.com>
2016-03-14 11:39:07 +0800

    Update install_head.sh

--- pub/install_head.sh
+++ pub/install_head.sh
@@ -5,7 +5,7 @@ LOG=$CURR/install.log
 echo "Extract package......"
 line=`wc -l $0|awk '{print $1}'`
 line=`expr $line - 15` 
-rm -r io-js/ Atlas/ node-red/ lib/ /configure/ edison_tools/ README.md install.sh install_for_dev.sh  
+rm -r io-js/ Atlas/ node-red/ lib/ configure/ edison_tools/ README.md install.sh install_for_dev.sh  
 tail -n $line $0 |tar xzv >> $LOG
 echo "Done!"
 ./install.sh
