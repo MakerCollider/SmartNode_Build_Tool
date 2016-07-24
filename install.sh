a18a7a198c52c3e3ed1e8ed3d43f3da80e113be2
TianYe <flymaxty@gmail.com>
2016-07-24 22:23:57 +0800

    update install script.

--- install.sh
+++ install.sh
@@ -1,13 +1,4 @@
-cat << "EOF"
-
-            __  __       _                ____      _ _ _     _
-           |  \/  | __ _| | _____ _ __   / ___|___ | | (_) __| | ___ _ __
-           | |\/| |/ _` | |/ / _ \ '__| | |   / _ \| | | |/ _` |/ _ \ '__|
-           | |  | | (_| |   <  __/ |    | |__| (_) | | | | (_| |  __/ |
-           |_|  |_|\__,_|_|\_\___|_|     \____\___/|_|_|_|\__,_|\___|_|
-
-
-EOF
+#!/bin/sh
 
 CURR=`pwd`
 LOG=$CURR/install.log
@@ -15,59 +6,62 @@ LOG=$CURR/install.log
 echo "## 1    ## Import configure"
 cp $CURR/configure/asound.conf /etc/
 cp $CURR/configure/asound.state /var/lib/alsa/
-cp $CURR/configure/timesyncd.conf /etc/systemd/
+#cp $CURR/configure/timesyncd.conf /etc/systemd/
+cp $CURR/configure/mosquitto.conf /etc/mosquitto/
 
 echo "## 1.1  ## Apply configure"
 alsactl restore
 timedatectl set-timezone Asia/Hong_Kong
+systemctl restart mosquitto
 
-echo "## 2    ## Update mraa"
-opkg install $CURR/lib/mraa_0.9.0_i586.ipk
+echo "## 2    ## Update mraa(disabled)"
+#opkg install $CURR/lib/mraa_0.9.0_i586.ipk
 echo "done"
 
-echo "## 3    ## Update upm"
-opkg install $CURR/lib/upm_0.4.1_i586.ipk
+echo "## 3    ## Update upm(disabled)"
+#opkg install $CURR/lib/upm_0.4.1_i586.ipk
 echo "done"
 
-echo "## 4    ## Install git"
-opkg install $CURR/lib/git_2.5.0-r0_core2-32.ipk
+echo "## 4    ## Install git(disabled)"
+#opkg install $CURR/lib/git_2.5.0-r0_core2-32.ipk
 
 echo "## 5    ## Install opencv"
-#opkg install $CURR/lib/libc6/libc6_2.20-r0.0_core2-32.ipk
-#opkg install $CURR/lib/libc6/libc6-dev_2.20-r0.0_core2-32.ipk
-#opkg install $CURR/lib/libc6/libc6-dbg_2.20-r0.0_core2-32.ipk
-opkg install $CURR/lib/libjpeg8_8d+1.3.1-r0.0_core2-32.ipk
-#opkg install $CURR/lib/libjpeg/libjpeg-dev_8d+1.3.1-r0.0_core2-32.ipk
-#opkg install $CURR/lib/libjpeg/libjpeg-dbg_8d+1.3.1-r0.0_core2-32.ipk
-opkg install $CURR/lib/libv4l_1.0.1-r0.0_core2-32.ipk
-opkg install $CURR/lib/libv4l-dev_1.0.1-r0.0_core2-32.ipk
-#opkg install $CURR/lib/libv4l/libv4l-dbg_1.0.1-r0.0_core2-32.ipk
-opkg install $CURR/lib/OpenCV/opencv_3.0-r0_core2-32.ipk
-opkg install $CURR/lib/OpenCV/opencv-link_3.0-r0_core2-32.ipk
-opkg install $CURR/lib/OpenCV/opencv-dev_3.0-r0_core2-32.ipk
-#opkg install $CURR/lib/OpenCV/opencv-dbg_3.0-r0_core2-32.ipk
-opkg install $CURR/lib/OpenCV/opencv-staticdev_3.0-r0_core2-32.ipk
+mkdir -p /usr/local
+opkg install $CURR/lib/opencv/libpng16-16_1.6.13-r0_core2-32.ipk
+opkg install $CURR/lib/opencv/libpng16-dev_1.6.13-r0_core2-32.ipk
+opkg install $CURR/lib/opencv/libgif-dev_4.1.6-r3_core2-32.ipk
+opkg install $CURR/lib/opencv/libgif4_4.1.6-r3_core2-32.ipk
+opkg install $CURR/lib/opencv/libtiff-dev_4.0.3-r0_core2-32.ipk
+opkg install $CURR/lib/opencv/libtiff5_4.0.3-r0_core2-32.ipk
+opkg install $CURR/lib/opencv/libv4l-dev_1.0.1-r0_core2-32.ipk
+opkg install $CURR/lib/opencv/libv4l_1.0.1-r0_core2-32.ipk
+opkg install $CURR/lib/opencv/libwebp-dev_0.4.0-r0_core2-32.ipk
+opkg install $CURR/lib/opencv/libwebp_0.4.0-r0_core2-32.ipk
+opkg install $CURR/lib/opencv/v4l-utils-dev_1.0.1-r0_core2-32.ipk
+opkg install $CURR/lib/opencv/v4l-utils_1.0.1-r0_core2-32.ipk
+sh $CURR/lib/opencv/OpenCV-3.1.0-887-g0015a09-i686.sh --prefix=/usr/local --exclude-subdir
 
 echo "## 6    ## Install sox"
 #tar -xzvf $CURR/lib/sox.tar.gz -C /usr >> $LOG
-opkg install $CURR/lib/libx264-133_r2265+git0+ffc3ad4945-r0_core2-32.ipk
-opkg install $CURR/lib/libtheora_1.1.1-r1_core2-32.ipk
-opkg install $CURR/lib/libavutil51_0.8.15-r0_core2-32.ipk
-opkg install $CURR/lib/libavcodec53_0.8.15-r0_core2-32.ipk
-opkg install $CURR/lib/libavformat53_0.8.15-r0_core2-32.ipk
-opkg install $CURR/lib/libpng16-16_1.6.13-r0_core2-32.ipk
-opkg install $CURR/lib/sox_14.4.0-r1_core2-32.ipk
+opkg install $CURR/lib/sox/libx264-133_r2265+git0+ffc3ad4945-r0_core2-32.ipk
+opkg install $CURR/lib/sox/libtheora_1.1.1-r1_core2-32.ipk
+opkg install $CURR/lib/sox/libavutil51_0.8.15-r0_core2-32.ipk
+opkg install $CURR/lib/sox/libavcodec53_0.8.15-r0_core2-32.ipk
+opkg install $CURR/lib/sox/libavformat53_0.8.15-r0_core2-32.ipk
+opkg install $CURR/lib/sox/libpng16-16_1.6.13-r0_core2-32.ipk
+opkg install $CURR/lib/sox/sox_14.4.0-r1_core2-32.ipk
 
 echo "## 7    ## Install mpg123"
 #tar -xzvf $CURR/lib/mpg123.tar.gz -C /usr >> $LOG
-opkg install $CURR/lib/mpg123_1.22.4-r0_core2-32.ipk
+opkg install $CURR/lib/mpg123/mpg123_1.22.4-r0_core2-32.ipk
 
 echo "## 8    ## Install festival"
 #tar -xzvf $CURR/lib/festival_prebuild.tar.gz -C /opt >> $LOG
 #cp /opt/festival/festival/bin/festival /usr/bin
-opkg install $CURR/lib/festival_2.3-r0_core2-32.ipk
+opkg install $CURR/lib/festival/festival_2.3-r0_core2-32.ipk
 
 echo "## 9    ## Hacking mraa-diy library"
+opkg remove mraa mraa-dev --force-depends
 tar -xzvf $CURR/lib/mraa-diy.tgz -C /usr >> $LOG
 #cp -r /usr/lib/node_modules/mraa/mraa.node $CURR/node-red/node_modules/mraa/build/Release/
 
@@ -79,13 +73,20 @@ cd $CURR/edison_tools
 ./install_edisontools.sh
 cd $CURR
 
-echo "## 12   ## Install mqtt package"
-opkg install $CURR/lib/mqtt_1.4/*.ipk
+echo "## 12   ## Install mqtt package(disabled)"
+#opkg install $CURR/lib/mqtt_1.4/*.ipk
 
-echo "## 12.1 ## Config mqtt"
-cp $CURR/lib/mqtt_1.4/mosquitto.conf /etc/mosquitto/
+echo "## 12.1 ## Config mqtt(disabled, move to stage 1.1)"
+#cp $CURR/lib/mqtt_1.4/mosquitto.conf /etc/mosquitto/
 
-echo "## 13   ## Set smartnode service"
+echo "## 13   ## Add showip service"
+mkdir /etc/init.d
+cp $CURR/lib/showip /etc/init.d
+chmod 755 /etc/init.d/showip
+update-rc.d -f showip remove
+update-rc.d showip defaults 91
+
+echo "## 14   ## Set smartnode service"
 SERVICE=/etc/systemd/system/nodered.service
 rm -rf $SERVICE
 echo "[Unit]" > $SERVICE
@@ -100,19 +101,12 @@ echo "RestartSec=1" >> $SERVICE
 echo "[Install]" >> $SERVICE
 echo "WantedBy=multi-user.target" >> $SERVICE
 
-echo "## 13.1 ## Start smartnode service"
+echo "## 14.1 ## Start smartnode service"
 sleep 1
 systemctl disable nodered
 sleep 1
 systemctl enable nodered 
 sleep 1
 systemctl restart nodered > /dev/null 2>&1
-
-echo "## 14   ## Add showip service"
-mkdir /etc/init.d
-cp $CURR/lib/showip /etc/init.d
-chmod 755 /etc/init.d/showip
-update-rc.d -f showip remove
-update-rc.d showip defaults 91
 
 echo "## 15   ## All finish, log saved to $LOG"
--- pub/install_head.sh
+++ pub/install_head.sh
@@ -1,13 +1,24 @@
 #!/bin/sh
+
+cat << "EOF"
+
+            __  __       _                ____      _ _ _     _
+           |  \/  | __ _| | _____ _ __   / ___|___ | | (_) __| | ___ _ __
+           | |\/| |/ _` | |/ / _ \ '__| | |   / _ \| | | |/ _` |/ _ \ '__|
+           | |  | | (_| |   <  __/ |    | |__| (_) | | | | (_| |  __/ |
+           |_|  |_|\__,_|_|\_\___|_|     \____\___/|_|_|_|\__,_|\___|_|
+
+
+EOF
+
 CURR=`pwd`
 LOG=$CURR/install.log
 
-echo "Extract package......"
+echo "## 0    ## Extract package"
 line=`wc -l $0|awk '{print $1}'`
 line=`expr $line - 15` 
 rm -rf node-red/ lib/ configure/ edison_tools/ README.md install.sh sn_dev.sh
 tail -n $line $0 |tar xzv >> $LOG
-echo "Done!"
 ./install.sh
 ret=$?
 #
