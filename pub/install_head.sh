7e026464e01cd8a39c73a410505c684073caf5db
TianYe <flymaxty@gmail.com>
2016-07-27 19:41:30 +0800

    use new logo

--- pub/install_head.sh
+++ pub/install_head.sh
@@ -2,12 +2,13 @@
 
 cat << "EOF"
 
-            __  __       _                ____      _ _ _     _
-           |  \/  | __ _| | _____ _ __   / ___|___ | | (_) __| | ___ _ __
-           | |\/| |/ _` | |/ / _ \ '__| | |   / _ \| | | |/ _` |/ _ \ '__|
-           | |  | | (_| |   <  __/ |    | |__| (_) | | | | (_| |  __/ |
-           |_|  |_|\__,_|_|\_\___|_|     \____\___/|_|_|_|\__,_|\___|_|
+              ____                       _     _   _           _      
+             / ___| _ __ ___   __ _ _ __| |_  | \ | | ___   __| | ___ 
+             \___ \| '_ ` _ \ / _` | '__| __| |  \| |/ _ \ / _` |/ _ \
+              ___) | | | | | | (_| | |  | |_  | |\  | (_) | (_| |  __/
+             |____/|_| |_| |_|\__,_|_|   \__| |_| \_|\___/ \__,_|\___|
 
+            Copyright (C) 2015-2016, Maker Collider, All Rights Reserved.
 
 EOF
 
@@ -16,7 +17,7 @@ LOG=$CURR/install.log
 
 echo "## 0    ## Extract package"
 line=`wc -l $0|awk '{print $1}'`
-line=`expr $line - 26` 
+line=`expr $line - 27` 
 rm -rf node-red/ lib/ configure/ edison_tools/ README.md install.sh sn_dev.sh
 tail -n $line $0 |tar xzv >> $LOG
 ./install.sh
